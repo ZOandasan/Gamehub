a5d4af13598ce74a79c66f8517544c9e1a299809
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-28 13:29:25 -0700

    Settings Page Buttons

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -2,10 +2,26 @@
 
 export default function SettingsPage(){
     return (
-    <>
+    <div>
+        <h1>Settings Page</h1>
         <div>
-            <h1>Settings Page</h1>
+            <h3>Sizing</h3>
+            <div>
+                <button>Small</button>
+                <button>Regular</button>
+                <button>Large</button>
+                <button>Largest</button>
+            </div>
         </div>
-    </>
+        <div>
+            <h3>Color Theme</h3>
+            <div>
+                <button>Dark Theme</button>
+                <button>Alt Theme</button>
+                <button>Alt Theme 2</button>
+                <button>Default</button>
+            </div>
+        </div>
+    </div>
     );
 };
