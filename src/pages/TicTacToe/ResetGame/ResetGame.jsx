f5a6e01d22d6001f47fb46b64040ad1827b2816e
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-28 14:26:24 -0700

    Tic-Tac-Toe Reset

--- src/pages/TicTacToe/ResetGame/ResetGame.jsx
+++ src/pages/TicTacToe/ResetGame/ResetGame.jsx
@@ -1,5 +1,28 @@
 
 
-export default function ResetGame(){
-    return <></>;
+export default function ResetGame({ resetGame, play }){
+    return (
+        <>
+        {play ?
+            <></>
+        :
+            <section>
+                <button id="replay" onClick={resetGame}>Reset Game</button>
+            </section>
+        }
+        </>
+    )
+
+
+    if (!play) {
+        return (
+          <section>
+            <button id="replay" onClick={resetGame}>
+              Reset Game
+            </button>
+          </section>
+        );
+      } else {
+        return <></>;
+      }
 }
