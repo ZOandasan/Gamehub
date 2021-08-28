48b510466933a35b65cc620a7e2e09acf9b58639
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-08-28 13:36:07 -0700

    About Page Content

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,10 +2,29 @@
 
 export default function AboutPage(){
     return (
-    <>
+    <div>
+        <h1>About Page</h1>
         <div>
-            <h1>About Page</h1>
+            <p>Insert About Information here:</p>
         </div>
-    </>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Section</th>
+                        <th>Linkedin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Zach Oandasan</td>
+                        <td>Blackjack</td>
+                        <td><a herf="">Link</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
     );
 };
