36054902ad1f532639efde1310854dad80011014
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-09-07 11:48:39 -0700

    Signup/Login

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,8 +8,8 @@ export default function AuthPage({ setUser }) {
 
   return (
     <main>
-      <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up': 'Log In'}</button>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up': 'Log In'}</button>
     </main>
   );
 }
