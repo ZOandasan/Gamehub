833f1c758995504ed3e7cd7a513165ebe72b8cf4
Zach Oandasan <zach@Zachs-Mac-mini.local>
2021-09-07 11:52:50 -0700

    Disable Simon

--- src/pages/GamesIndexPage/GamesIndexPage.jsx
+++ src/pages/GamesIndexPage/GamesIndexPage.jsx
@@ -12,7 +12,7 @@ export default function GamesIndexPage(){
             <span><Link to="/connect-four"><button className="gameLink">Connect 4</button></Link></span>
             <span><Link to="/jotto"><button className="gameLink">Jotto</button></Link></span>
             <span><Link to="/tic-tac-toe"><button className="gameLink">Tic-Tac-Toe</button></Link></span>
-            <span><Link to="/simon"><button className="gameLink">Simon</button></Link></span>
+            {/* <span><Link to="/simon"><button className="gameLink">Simon</button></Link></span> */}
         </div>
     </>
     );
