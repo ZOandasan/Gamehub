48b0c17759f3e08c798c65cabc795d30c54b1648
ZOandasan <andsorez@gmail.com>
2021-09-08 11:36:42 -0700

    Styling Changes

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -28,18 +28,18 @@ export default function SettingsPage(){
         document.documentElement.style.setProperty('--border', 'rgb(250, 250, 250)');
     }
 
-    function setNewTheme(){
-        document.documentElement.style.setProperty('--background', '#292929');
-        document.documentElement.style.setProperty('--background-form', '#3145b8');
-        document.documentElement.style.setProperty('--background-title', '#3145b8');
-        document.documentElement.style.setProperty('--background-card', '#525252');
-        document.documentElement.style.setProperty('--button', '#6175b8');
-        document.documentElement.style.setProperty('--button-hover', '#5badae');
-        document.documentElement.style.setProperty('--header-text', 'rgb(240, 240, 240)');
-        document.documentElement.style.setProperty('--plain-text', 'rgb(240, 240, 240)');
-        document.documentElement.style.setProperty('--link', 'rgb(200, 250, 150)');
-        document.documentElement.style.setProperty('--link-visited', 'rgb(150, 250, 200)');
-        document.documentElement.style.setProperty('--border', 'rgb(250, 250, 250)');
+    function setSEITheme(){
+        document.documentElement.style.setProperty('--background', 'rgb(250, 250, 250)');
+        document.documentElement.style.setProperty('--background-form', '#fecb65');
+        document.documentElement.style.setProperty('--background-title', '#97cdb4');
+        document.documentElement.style.setProperty('--background-card', '#ffecaf');
+        document.documentElement.style.setProperty('--button', '#99ec65');
+        document.documentElement.style.setProperty('--button-hover', '#99ee99');
+        document.documentElement.style.setProperty('--header-text', 'rgb(0, 0, 0)');
+        document.documentElement.style.setProperty('--plain-text', 'rgb(0, 0, 0)');
+        document.documentElement.style.setProperty('--link', 'rgb(50, 0, 100)');
+        document.documentElement.style.setProperty('--link-visited', 'rgb(100, 0, 50)');
+        document.documentElement.style.setProperty('--border', 'rgb(0, 0, 0)');
     }
 
 
@@ -51,6 +51,7 @@ export default function SettingsPage(){
             <div>
                 <button onClick={() => setLight()}>Default Theme</button>
                 <button onClick={() => setDark()}>Dark Theme</button>
+                <button onClick={() => setSEITheme()}>Cafe Theme</button>
                 {/*
                 <button onClick={() => setNewTheme()}>Alt Theme</button>
                 */}
