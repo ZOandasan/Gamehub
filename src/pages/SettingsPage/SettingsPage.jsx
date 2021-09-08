1f5fc7916992142b61df8353c682d21f6c933ff3
ZOandasan <andsorez@gmail.com>
2021-09-08 11:25:51 -0700

    New Theme

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -28,6 +28,20 @@ export default function SettingsPage(){
         document.documentElement.style.setProperty('--border', 'rgb(250, 250, 250)');
     }
 
+    function setNewTheme(){
+        document.documentElement.style.setProperty('--background', '#292929');
+        document.documentElement.style.setProperty('--background-form', '#3145b8');
+        document.documentElement.style.setProperty('--background-title', '#3145b8');
+        document.documentElement.style.setProperty('--background-card', '#525252');
+        document.documentElement.style.setProperty('--button', '#6175b8');
+        document.documentElement.style.setProperty('--button-hover', '#5badae');
+        document.documentElement.style.setProperty('--header-text', 'rgb(240, 240, 240)');
+        document.documentElement.style.setProperty('--plain-text', 'rgb(240, 240, 240)');
+        document.documentElement.style.setProperty('--link', 'rgb(200, 250, 150)');
+        document.documentElement.style.setProperty('--link-visited', 'rgb(150, 250, 200)');
+        document.documentElement.style.setProperty('--border', 'rgb(250, 250, 250)');
+    }
+
 
     return (
     <div>
@@ -37,10 +51,9 @@ export default function SettingsPage(){
             <div>
                 <button onClick={() => setLight()}>Default Theme</button>
                 <button onClick={() => setDark()}>Dark Theme</button>
-                {/* <button>Alt Theme</button>
-                <button>Alt Theme 2</button>
-                <button>Default</button>
-                <button>Default</button> */}
+                {/*
+                <button onClick={() => setNewTheme()}>Alt Theme</button>
+                */}
             </div>
         </div>
     </div>
